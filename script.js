56f27c7823b967e03548e87e8c843ec959f6a4dc
Valerie Russell <vruss14@gmail.com>
2021-03-16 14:18:56 -0400

    added play again option and tally of previous game results

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
 let startBtn = document.getElementById("start-btn");
 
+let modal = document.createElement("modal");
+modal.setAttribute("class", "modal-style");
+modal.setAttribute("id", "game-modal");
+modal.setAttribute("style", "display: none;");
+document.body.children[1].append(modal);
+
+let wins = 0;
+let ties = 0;
+let losses = 0;
+
 startBtn.addEventListener("click", openGame);
+
+
+// Creates first screen with user options
 
 function openGame () {
 
-    let modal = document.createElement("modal");
-    modal.setAttribute("class", "modal-style");
-    modal.setAttribute("id", "game-modal");
-    document.body.children[1].append(modal);
+    modal.setAttribute("style", "display: flex;");
+    startBtn.setAttribute("style", "display: none");
+
+    document.body.setAttribute("style", "background-color: gray");
 
     let heading = document.createElement("h1");
     heading.setAttribute("class", "game-header");
     heading.textContent = "Rock, Paper, Scissors";
     modal.append(heading);
 
-    let gameDescr = document.createElement("p");
+    let gameDescr = document.createElement("h3");
     gameDescr.setAttribute("class", "paragraphs");
     gameDescr.textContent = "Battle the computer by selecting rock, paper, or scissors!";
     modal.append(gameDescr);
@@ -41,6 +54,8 @@ function openGame () {
     
 }
 
+// Reads user input
+
 let main = $("#content-wrapper");
 main.on("click", ".option-btns", runResults);
 
@@ -50,54 +65,50 @@ function runResults(event) {
     runComputerMove(userMove);
 }
 
+// Compares user input and computer output to determine win/tie/loss
+
 function runComputerMove (userMove) {
     document.getElementById("game-modal").innerHTML = "";
 
     let modalTwo = document.getElementById("game-modal");
-
     let options = ["Rock", "Paper", "Scissors"];
-
     let computerMove = options[Math.floor(Math.random() * options.length)];
-
-    console.log(computerMove);
 
     let computerResult = document.createElement("h1");
     computerResult.setAttribute("class", "game-header");
     computerResult.textContent = "The computer chose: " + computerMove;
     modalTwo.appendChild(computerResult);
 
-    console.log(userMove);
-
     if (userMove == computerMove) {
         let tieText = document.createElement("h2");
         tieText.setAttribute("class", "result-text");
         tieText.textContent = "It's a tie!";
         modalTwo.appendChild(tieText);
-        console.log("Tie");
+        ties++;
     } else if (userMove == "Rock" && computerMove == "Scissors") {
         let winText = document.createElement("h2");
         winText.setAttribute("class", "result-text");
         winText.textContent = "You win!";
         modalTwo.appendChild(winText);
-        console.log("Win");
+        wins++;
     } else if (userMove == "Paper" && computerMove == "Rock") {
         let winText = document.createElement("h2");
         winText.setAttribute("class", "result-text");
         winText.textContent = "You win!";
         modalTwo.appendChild(winText);
-        console.log("Win");
+        wins++;
     } else if (userMove == "Scissors" && computerMove == "Paper") {
         let winText = document.createElement("h2");
         winText.setAttribute("class", "result-text");
         winText.textContent = "You win!";
         modalTwo.appendChild(winText);
-        console.log("Win");
+        wins++;
     } else {
         let loseText = document.createElement("h2");
         loseText.setAttribute("class", "result-text");
         loseText.textContent = "You lose!";
         modalTwo.appendChild(loseText);
-        console.log("Lose");
+        losses++;
     }
 
     let playAgainBtn = document.createElement("button");
@@ -105,11 +116,29 @@ function runComputerMove (userMove) {
     playAgainBtn.textContent = "Play again?";
     modalTwo.appendChild(playAgainBtn);
 
+    // Results are changed each time the game is played
+
+    let winNumber = document.createElement("h3");
+    winNumber.setAttribute("class", "paragraphs");
+    winNumber.textContent = "Wins: " + wins;
+    modalTwo.appendChild(winNumber);
+
+    let tieNumber = document.createElement("h3");
+    tieNumber.setAttribute("class", "paragraphs");
+    tieNumber.textContent = "Ties: " + ties;
+    modalTwo.appendChild(tieNumber);
+
+    let loseNumber = document.createElement("h3");
+    loseNumber.setAttribute("class", "paragraphs");
+    loseNumber.textContent = "Losses: " + losses;
+    modalTwo.appendChild(loseNumber);
 }
 
-// main.on("click", ".again-btn", runAgain);
+main.on("click", ".again-btn", runAgain);
 
-// function runAgain() {
-//     document.getElementById("game-modal").innerHTML = "";
-//     openGame();
-// }
+// To play the game again
+
+function runAgain() {
+    document.getElementById("game-modal").innerHTML = "";
+    openGame();
+}
